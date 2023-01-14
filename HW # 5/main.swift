c79dfab8fbf4fb7c90a9cdae3c2964cf8d6ac229
Memka <mkasenova87@gmail.com>
2023-01-14 19:23:24 +0600

    изм

--- HW # 5/main.swift
+++ HW # 5/main.swift
@@ -16,16 +16,17 @@ var basket:[String:Int] = [:]
 basket["Milk"] = 45
 basket["Bread"] = 76
 basket["Cake"] = 489
-
+//["Cake": 489, "Bread": 76, "Milk": 45]
 
 print(basket)
 
-var sum = (45+76+489)
+var sum = 0
 for number in basket {
-sum += Int()
+    sum += number.value
 }
 
-print("Итого",sum ,"сом")
+print("Итого",sum ,"сом") //Итого 610 сом
+
 
 
 
@@ -41,84 +42,36 @@ print("Итого",sum ,"сом")
 // Итого: 218 сом
 
 
-
 var tovar:[String:Int] = ["Cola":50, "Kefir":65, "Milk":40, "Bread":20, "Salad":100, "Sugar":38, "Pepper":16, "Fanta": 58,"Cheese":90, "Potato":34]
 
 var searchTovar = readLine()!
 var searchTovar2 = readLine()!
 var searchTovar3 = readLine()!
-searchTovar
 
 
+print("-----Чек-----")
+print("Наим-е:    Сумма:")
 
-//for (key, value) in tovar(){
-//  print(value)
-//}
-func priceTovar(tovar: String)->Int{
-        switch tovar {
-        case "Cola":
-            print("Cola - 50 cом")
-            return 50
-        case "Kefir":
-            print("Kefir - 65 cом")
-            return 65
-        case "Milk":
-            print("Milk - 40 cом")
-            return 40
-        case "Bread":
-            print("Bread - 20 cом")
-            return 20
-        case "Salad":
-            print("Salad - 100 cом")
-            return 100
-        case "Sugar":
-            print("Sugar - 38 cом")
-            return 38
-        case "Pepper":
-            print("Pepper - 16 cом")
-            return 16
-        case "Fanta":
-            print("Fanta - 58 cом")
-            return 58
-        case "Cheese":
-            print("Cheese - 90 сом")
-            return 90
-        case "Potato":
-            print("Potato - 34 cом")
-            return 34
-        default:
-            print("Нет в чеке!")
-            return 0
+
+func priceProduct(_ p1: String, _ p2: String, _ p3: String){
+    var price1 = 0
+    var price2 = 0
+    var price3 = 0
+    for i in tovar {
+        if i.key == p1 {
+            print("Продукт: \(i.key), цена: \(i.value)")
+            price1 = i.value
+        }
+        if i.key == p2 {
+            print("Продукт: \(i.key), цена: \(i.value)")
+            price2 = i.value
+        }
+        if i.key == p3 {
+            print("Продукт: \(i.key), цена: \(i.value)")
+            price3 = i.value
         }
     }
-    print("-----Чек-----")
-    print("Наим-е:    Сумма:")
-
-    var Cola = readLine()!
-    var Kefir = readLine()!
-    var Milk = readLine()!
-    var Bread = readLine()!
-    var Salad = readLine()!
-    var Sugar = readLine()!
-    var Pepper = readLine()!
-    var Fanta = readLine()!
-    var Cheese = readLine()!
-    var Potato = readLine()!
-
-    
-    var ColaPrice = priceTovar(tovar: Cola)
-    var KefirPrice = priceTovar(tovar: Kefir)
-    var MilkPrice = priceTovar(tovar: Milk)
-    var BreadPrice = priceTovar(tovar: Bread)
-    var SaladPrice = priceTovar(tovar: Salad)
-    var SugarPrice = priceTovar(tovar: Sugar)
-    var PepperPrice = priceTovar(tovar: Pepper)
-    var FantaPrice = priceTovar(tovar: Fanta)
-    var CheesePrice = priceTovar(tovar: Cheese)
-    var PotatoPrice = priceTovar(tovar: Potato)
-
-    var totalPrice = ColaPrice + KefirPrice + MilkPrice + BreadPrice + SaladPrice + SugarPrice + PepperPrice + FantaPrice + CheesePrice + PotatoPrice
-
-
     print("-------------------")
-    print("Total: \(totalPrice) сом")
+    print("Итого:\(price1 + price2 + price3) сом")
+}
+
